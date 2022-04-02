5909f079c2dedaa493987f993afa53784df30006
Andreas Reischl <andistar88@gmx.at>
2022-04-02 23:00:55 +0200

    shooting code added, but not yet validated

--- src/at/fhooe/ai/mcm/g1/FirstRobot.java
+++ src/at/fhooe/ai/mcm/g1/FirstRobot.java
@@ -1,19 +1,14 @@
 package at.fhooe.ai.mcm.g1;
 
-import java.awt.Color;
-
 import robocode.AdvancedRobot;
 import robocode.ScannedRobotEvent;
 
 public class FirstRobot extends AdvancedRobot {
-	EnemyRobot r = new EnemyRobot();
-	
 	@Override
 	public void run() {
 		setAdjustGunForRobotTurn(true);
 		setAdjustRadarForGunTurn(true);
 		setTurnRadarRight(Double.POSITIVE_INFINITY);
-		
 
 		while(true) {
 			ahead(100);
@@ -25,19 +20,20 @@ public class FirstRobot extends AdvancedRobot {
 	
 	@Override
 	public void onScannedRobot(ScannedRobotEvent event) {
-		// TODO Auto-generated method stub
-		super.onScannedRobot(event);
-		this.r = new EnemyRobot(event, getX(), getY());
+		EnemyRobot r1 = new EnemyRobot(event, getX(), getY());
+		double time = 1; // 1 tick
 		
+		double distanceEnemyTurn = r1.getVelocity() * time;
+		double XEnemyNew = r1.getScannedX() + Math.sin(r1.getAngle()) * distanceEnemyTurn;
+		double YEnemyNew = r1.getScannedY() + Math.cos(r1.getAngle()) * distanceEnemyTurn;
+		
+		double distanceBullet = Math.sqrt(Math.pow(XEnemyNew - getX(), 2) + Math.pow(YEnemyNew - getY(), 2));
+		
+		double angleBarrelRotation = Math.acos((XEnemyNew - getX()) / distanceBullet);
+		
+		double power = (distanceBullet / time - 20) / (-3);
+		
+		turnGunLeft(angleBarrelRotation); //might change it to not always use left rotation
+		fire(power);
 	}
-	
-	public void onPaint(java.awt.Graphics2D g) {
-		g.setColor(new Color(0xff, 0x00, 0x00, 0x80));
-
-	    // Draw a line from our robot to the scanned robot
-	    g.drawLine(r.getScannedX(), r.getScannedY(), (int)getX(), (int)getY());
-
-	    // Draw a filled square on top of the scanned robot that covers it
-	    g.fillRect(r.getScannedX() - 20, r.getScannedY() - 20, 40, 40);
-	};
 }
