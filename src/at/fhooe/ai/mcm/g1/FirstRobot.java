f2d29f2c8068cad6baccc2d8dd69d9ff33d70dc5
Lum1nat0r <thomas-lumesberger@live.com>
2022-04-03 09:36:24 +0200

    calculate distances to the wall implemented

--- src/at/fhooe/ai/mcm/g1/FirstRobot.java
+++ src/at/fhooe/ai/mcm/g1/FirstRobot.java
@@ -1,19 +1,33 @@
 package at.fhooe.ai.mcm.g1;
 
+import java.util.Iterator;
+
 import robocode.AdvancedRobot;
 import robocode.ScannedRobotEvent;
 
 public class FirstRobot extends AdvancedRobot {
 	@Override
 	public void run() {
+		
+		double BFHeight = getBattleFieldHeight();
+		double BFWidth = getBattleFieldWidth();
+
+		double rotation = getHeading();
+
+
+		double X = getX();
+		double Y = getY();
+		
+		double distances[] = calcDistances(BFHeight, BFWidth, X, Y);
+		
 		setAdjustGunForRobotTurn(true);
 		setAdjustRadarForGunTurn(true);
 		setTurnRadarRight(Double.POSITIVE_INFINITY);
 
-		while(true) {
-			ahead(100);
-			turnLeft(90);
-		}
+//		while(true) {
+//			ahead(100);
+//			turnLeft(90);
+//		}
 		
 	}
 
@@ -22,5 +36,11 @@ public class FirstRobot extends AdvancedRobot {
 	public void onScannedRobot(ScannedRobotEvent event) {
 		EnemyRobot r1 = new EnemyRobot(event);
 		
+	}
+	
+	public double[] calcDistances(double BFHeight, double BFWidth, double X, double Y) {
+		// distances in order: Top, Right, Bottom, Left
+		double distances[] = {BFHeight - Y, BFWidth- X, Y, X};
+		return distances;
 	}
 }
